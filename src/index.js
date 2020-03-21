1ab397861f2ba62f172b7d1828b25570f8b4e3d3
nikita <zotiken@gmail.com>
2020-03-21 13:23:50 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-// import store from './state/state'
-import store from './Redux/storeRedux'
+import store from './state/state'
+// import store from './Redux/storeRedux'
 
 
 function reRenender () {
